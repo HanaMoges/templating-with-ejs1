32cd4007ef701bcd62b38223a560fedbe52e6c2a
Josue Nunez <106881652+Josue-Nunez@users.noreply.github.com>
2023-03-10 01:39:05 +0000

    adds main functionality

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set(`view engine`, `ejs`);
+app.use(express.static(__dirname));
+
+
+var data = require('./data/test.json');
+
+app.get('/', (req, res) => {
+  var title = "Home Page";
+  res.render('pages/index', { 'title': title });
+});
+
+// app.get('/about', (req, res) => {
+//   var title = "About Page";
+//   res.render('pages/about', { 'title': title });
+// });
+
+app.get('/kali', (req, res) => {
+  var title = "Indian Goddess Kali";
+  res.render('pages/kali', { 'title': title });
+});
+
+app.get('/volcarona', (req, res) => {
+  var title = "Pokemon: Volcarona";
+  res.render('pages/volcarona', { 'title': title });
+});
+
+app.get('/yukari', (req, res) => {
+  var title = "Yukari the Youkai";
+  res.render('pages/yukari', { 'title': title });
+});
+
+app.get('/users', function(req, res) {
+  var title = 'Users Page';
+  res.render('users/index', {
+    title: title,
+    users: data
+  });
+});
+
+app.get('/users/view/:id', function(req, res) {
+  var title = 'User Page';
+  var id = req.params.id;
+  res.render('users/view', {
+    title: title,
+    user: data[--id]
+  });
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  console.log(data);
+});
